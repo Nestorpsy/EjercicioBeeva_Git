0796b0ce8e463b7b2c3f6dfdcc7bc9ec7eb4bc58
nestorpsy <nestorpsy@hotmail.com>
2017-07-07 16:37:09 -0500

    Signed-off-by: nestorpsy <nestorpsy@hotmail.com>

--- BancoApp_Spring/src/main/java/com/beeva/app/utilerias/JDBCUtils.java
+++ BancoApp_Spring/src/main/java/com/beeva/app/utilerias/JDBCUtils.java
@@ -1,5 +1,19 @@
 package com.beeva.app.utilerias;
 
-public class JDBCUtils {
+import java.sql.Connection;
+import java.sql.DriverManager;
 
+public class JDBCUtils {
+	public Connection conexion() {
+		Connection connection =null;    	
+    	try {
+			connection = DriverManager
+					.getConnection("jdbc:mysql://localhost:3306/prueba","root","root");		
+			return connection;
+		} catch (Exception e) {
+			System.out.println("Error de conexion X_X");
+			e.printStackTrace();
+			return null;
+		}
+	}
 }
