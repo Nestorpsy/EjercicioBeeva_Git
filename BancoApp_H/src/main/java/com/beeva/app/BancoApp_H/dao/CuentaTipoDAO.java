752fc47e392a81e0070422fae767eac0829ca92b
nestorpsy <nestorpsy@hotmail.com>
2017-07-17 16:55:53 -0500

    Signed-off-by: nestorpsy <nestorpsy@hotmail.com>

--- BancoApp_H/src/main/java/com/beeva/app/BancoApp_H/dao/CuentaTipoDAO.java
+++
@@ -1,8 +0,0 @@
-package com.beeva.app.bancoapp_h.dao;
-
-import com.beeva.app.bancoapp_h.modelo.Cuenta;
-
-public interface CuentaTipoDAO {
-	public Cuenta deposito(Cuenta cuenta,double dep);
-	public Cuenta retiro(Cuenta cuenta,double ret);	
-}
